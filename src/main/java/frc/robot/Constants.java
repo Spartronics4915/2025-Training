3579a19ff07548f4be1062c073f00bb3ca610a90
Finn <148401810+FinnSpartronics@users.noreply.github.com>
2025-09-30 10:55:29 -0700

    Simplify everything to be cleaner and simpler for new members

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,23 +4,10 @@
 
 package frc.robot;
 
-import edu.wpi.first.math.geometry.Rotation2d;
-
-/**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
- *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
- */
 public final class Constants {
 
-    public static class OperatorConstants {
-
-        public static final int kDriverControllerPort = 0;
-        public static final Rotation2d kStartingAngle = Rotation2d.fromDegrees(270);
-
+    public final class MotorConstants {
+        public static final int MOTOR_ID = 2;
     }
 
 }
--- src/main/java/frc/robot/subsystems/MotorSubsystem.java
+++ src/main/java/frc/robot/subsystems/MotorSubsystem.java
@@ -5,10 +5,10 @@ import com.ctre.phoenix6.configs.TalonFXConfigurator;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
-public class KrakenX60Run {
-    private TalonFX krakenX60Motor;
+import frc.robot.Constants;
 
-    public static final int motorID = 2;
+public class MotorSubsystem {
+    private TalonFX krakenX60Motor;
 
     public static final CurrentLimitsConfigs currentLimits = new CurrentLimitsConfigs()
             .withSupplyCurrentLimitEnable(true)
@@ -16,12 +16,12 @@ public class KrakenX60Run {
             .withSupplyCurrentLowerLimit(10)
             .withSupplyCurrentLowerTime(1.0);
 
-    public KrakenX60Run() {
+    public MotorSubsystem() {
         initMotor();
     }
 
     private void initMotor() {
-        krakenX60Motor = new TalonFX(motorID);
+        krakenX60Motor = new TalonFX(Constants.MotorConstants.MOTOR_ID);
 
         krakenX60Motor.setNeutralMode(NeutralModeValue.Brake);
         TalonFXConfigurator configurator = krakenX60Motor.getConfigurator();
